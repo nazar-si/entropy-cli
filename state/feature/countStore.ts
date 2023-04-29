4bea3b5f756267c3c3b93c8bb9c4085a7ec2335d
nazar-si <nazari61@yandex.ru>
2023-04-29 23:48:17 +0300

    refactor: countStore.ts

--- state/feature/countStore.ts
+++ state/feature/countStore.ts
@@ -1,13 +1,12 @@
-import { create } from "zustand"
+import { create } from "zustand";
 
 export interface State {
-    count: number
+  count: number;
 }
 
-const $countStore = create<State>(set => ({
-    count: 0,
-    add: () => set(s => ({ count: s.count + 1 }))
-}))
+const $countStore = create<State>((set) => ({
+  count: 0,
+  add: () => set((s) => ({ count: s.count + 1 })),
+}));
 
-
-export default $countStore 
+export default $countStore;
